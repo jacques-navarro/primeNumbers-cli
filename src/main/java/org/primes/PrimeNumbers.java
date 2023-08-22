2b714cec440df53975c54d89f6ed45198287d65a
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-08-22 12:16:17 +0200

    Add 'isPrime'

--- src/main/java/org/primes/PrimeNumbers.java
+++ src/main/java/org/primes/PrimeNumbers.java
@@ -3,7 +3,19 @@ package org.primes;
 public class PrimeNumbers {
 
     public static boolean isPrime(int n) {
-        return false;
+        if (n == 2) {
+            return true;
+        }
+
+        int midpoint =  (int) Math.sqrt(n);
+
+        for (int i = 2; i <= midpoint + 1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
