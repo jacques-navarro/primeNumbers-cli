81cc137482157af9f2c69cc6a85d445824d87fcc
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-08-22 12:14:24 +0200

    Refactor 'isPrime' tests, moved 2 to correct test

--- src/test/java/PrimeNumbersTest.java
+++ src/test/java/PrimeNumbersTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class PrimeNumbersTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {3, 5, 7, 11, 13, 19, 23, 29, 41, 101, 1_000_003})
+    @ValueSource(ints = {2, 3, 5, 7, 11, 13, 19, 23, 29, 41, 101, 1_000_003})
     @DisplayName("Test numbers that are prime")
     public void testIsPrime_ShouldReturnTrue(int testInput) {
         PrimeNumbers primeNumbers = new PrimeNumbers();
@@ -20,7 +20,7 @@ public class PrimeNumbersTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {2, 4, 6, 8, 9, 10, 27, 33, 39, 1_000_002})
+    @ValueSource(ints = {4, 6, 8, 9, 10, 27, 33, 39, 1_000_002})
     @DisplayName("Test numbers that are not prime")
     public void testIsPrime_ShouldReturnFalse(int testInput) {
         PrimeNumbers primeNumbers = new PrimeNumbers();
