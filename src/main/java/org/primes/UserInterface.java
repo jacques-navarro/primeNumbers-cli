fae57709c0843922106d042a45fe6cf6b759c02f
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-08-22 22:20:28 +0200

    Wrapped menu selection in try/catch NumberFormatException

--- src/main/java/org/primes/UserInterface.java
+++ src/main/java/org/primes/UserInterface.java
@@ -24,12 +24,18 @@ public class UserInterface {
 
             System.out.print("Enter choice: ");
 
-            int choice = Integer.valueOf(scanner.nextLine());
+            int choice;
+                try {
+                    choice = Integer.valueOf(scanner.nextLine());
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Make your choice by selecting a number between 1 and 3\n");
+                    continue;
+                }
 
             System.out.println();
 
             if (choice < 1 || choice > 3) {
-                System.out.println("Please enter a valid choice!");
+                System.out.println("Please enter a valid choice!\n");
                 continue;
             }
 
@@ -75,7 +81,7 @@ public class UserInterface {
                 done = true;
 
             } catch (NumberFormatException iae) {
-                System.out.printf("Enten a number smaller than %,d.\n", Integer.MAX_VALUE);
+                System.out.printf("Enten a number greater than 1 and smaller than %,d.\n", Integer.MAX_VALUE);
                 continue;
             } finally {
                 System.out.println();
@@ -113,7 +119,7 @@ public class UserInterface {
                 done = true;
 
             } catch (NumberFormatException iae) {
-                System.out.printf("Enten a number smaller than %,d.\n", Integer.MAX_VALUE);
+                System.out.printf("Enter a number greater than 2 and smaller than %,d.\n", Integer.MAX_VALUE);
                 continue;
             } finally {
                 System.out.println();
