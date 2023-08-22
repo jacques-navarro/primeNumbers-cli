ec230f5bb264f20de1ea47a554bfc2340c2cc5a4
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-08-22 11:48:37 +0200

    Add tests for 'isPrime' method

--- src/main/java/org/primes/PrimeNumbers.java
+++ src/main/java/org/primes/PrimeNumbers.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.primes;
 
 public class PrimeNumbers {
 
---
+++ src/test/java/PrimeNumbersTest.java
@@ -0,0 +1,32 @@
+import org.primes.PrimeNumbers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class PrimeNumbersTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 5, 7, 11, 13, 19, 23, 29, 41, 101, 1_000_003})
+    @DisplayName("Test numbers that are prime")
+    public void testIsPrime_ShouldReturnTrue(int testInput) {
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+
+        boolean actual = primeNumbers.isPrime(testInput);
+
+        assertTrue(actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8, 9, 10, 27, 33, 39, 1_000_002})
+    @DisplayName("Test numbers that are not prime")
+    public void testIsPrime_ShouldReturnFalse(int testInput) {
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+
+        boolean actual = primeNumbers.isPrime(testInput);
+
+        assertFalse(actual);
+    }
+}
